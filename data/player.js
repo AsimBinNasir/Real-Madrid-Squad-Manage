d46e68eb6a654efffe1b2bd5df14f7d8e7d31a41
AsimBinNasir <asimbinnasir2906@email.com>
2025-06-16 01:05:40 +0600

    feat: expand player.js with defender, midfielder, and forward arrays
    - Added arrays for defenders, midfielders, and forwards to player.js
    - Each array contains player objects with name, jersey number, image, and position
    - Improved data structure for easier access to all

---
+++ data/player.js
@@ -0,0 +1,149 @@
+export const players = {
+  goalkeeper: [
+    {
+      name: "COURTOIS",
+      jerseyNumber: 1,
+      image: "Images/Courtois.webp",
+      position: "Goalkeeper"
+    },
+    {
+      name: "LUNIN",
+      jerseyNumber: 13,
+      image: "Images/Lunin.webp",
+      position: "Goalkeeper"
+    }
+  ],
+  defender: [
+    {
+      name: "CARVAJAL",
+      jerseyNumber: 2,
+      image: "Images/Carvajal.webp",
+      position: "Defender"
+    },
+    {
+      name: "E.MILIT&Atilde;O",
+      jerseyNumber: 3,
+      image: "Images/E.Militao.webp",
+      position: "Defender"
+    },
+    {
+      name: "ALABA",
+      jerseyNumber: 4,
+      image: "Images/Alaba.webp",
+      position: "Defender"
+    },
+    {
+      name: "ALEXANDER",
+      jerseyNumber: 12,
+      image: "Images/Alexander.webp",
+      position: "Defender"
+    },
+    {
+      name: "LUCAS V.",
+      jerseyNumber: 17,
+      image: "Images/Lucas V.webp",
+      position: "Defender"
+    },
+    {
+      name: "FRAN GARC&Aacute;",
+      jerseyNumber: 20,
+      image: "Images/Fran Garcia.webp",
+      position: "Defender"
+    },
+    {
+      name: "R&Uuml;DIGER",
+      jerseyNumber: 22,
+      image: "Images/Rudiger.webp",
+      position: "Defender"
+    },
+    {
+      name: "F. MENDY",
+      jerseyNumber: 23,
+      image: "Images/F. Mendy.webp",
+      position: "Defender"
+    },
+    {
+      name: "HUIJSEN",
+      jerseyNumber: 24,
+      image: "Images/Huijsen.webp",
+      position: "Defender"
+    }
+
+  ],
+  midfielder: [
+    {
+      name: "BELLINGHAM",
+      jerseyNumber: 5,
+      image: "Images/Bellingham.webp",
+      position: "Midfielder"
+    },
+    {
+      name: "CAMAVINGA",
+      jerseyNumber: 6,
+      image: "Images/Camavinga.jpg",
+      position: "Midfielder"
+    },
+    {
+      name: "VALVERDE",
+      jerseyNumber: 8,
+      image: "Images/Valverde.jpg",
+      position: "Midfielder"
+    },
+    {
+      name: "MODRI&Cacute;",
+      jerseyNumber: 10,
+      image: "Images/Modric.jpg",
+      position: "Midfielder"
+    },
+    {
+      name: "TCHOUAM&Eacute;NI",
+      jerseyNumber: 18,
+      image: "Images/Tchouameni.jpg",
+      position: "Midfielder"
+    },
+    {
+      name: "A. G&Uum;lER",
+      jerseyNumber: 15,
+      image: "Images/Arda Guler.webp",
+      position: "Midfielder"
+    },
+    {
+      name: "D. CEBALLOS",
+      jerseyNumber: 19,
+      image: "Images/D. Ceballos.webp",
+      position: "Midfielder"
+    }
+  ],
+  forward: [
+    {
+      name: "VINICIUS JR.",
+      jerseyNumber: 7,
+      image: "Images/Vinicius.jpg",
+      position: "Forward"
+    },
+    {
+      name: "MBAPP&Eacute;",
+      jerseyNumber: 9,
+      image: "Images/MBappe.webp",
+      position: "Forward"
+    },
+    {
+      name: "RODRYGO",
+      jerseyNumber: 11,
+      image: "Images/Rodrygo.webp",
+      position: "Forward"
+    },
+    {
+      name: "ENDRICK",
+      jerseyNumber: 16,
+      image: "Images/Endrick.webp",
+      position: "Forward"
+    },
+    {
+      name: "BRAHIM",
+      jerseyNumber: 21,
+      image: "Images/Brahim.webp",
+      position: "Forward"
+    }
+  ]
+};
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hello world")
+import { players } from './data/player.js';
+
+// Access the goalkeeper array
+const goalkeepers = players.goalkeeper;
+const defenders = players.defender;
+const midfielders = players.midfielder;
+// Example: log the entire defender array
+console.log(midfielders);
+// Example: log all goalkeeper names
+goalkeepers.forEach(gk => {
+  console.log(gk.name);
+});
